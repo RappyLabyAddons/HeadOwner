93c3f13603d0021b3ba509a18d7075cb2844f2bb
RappyTV <rappytv@rappytv.com>
2023-08-18 12:29:44 +0200

    Fix copy button being hold and being pressed in menus

--- core/src/main/java/com/rappytv/headowner/listeners/KeyListener.java
+++ core/src/main/java/com/rappytv/headowner/listeners/KeyListener.java
@@ -4,6 +4,7 @@ import com.rappytv.headowner.HeadOwnerAddon;
 import net.labymod.api.Laby;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.input.KeyEvent;
+import net.labymod.api.event.client.input.KeyEvent.State;
 import net.labymod.api.util.I18n;
 
 public class KeyListener {
@@ -16,7 +17,7 @@ public class KeyListener {
 
     @Subscribe
     public void onKey(KeyEvent event) {
-        if(event.key() == addon.configuration().copyKey()) {
+        if(event.state() == State.PRESS && event.key() == addon.configuration().copyKey() && !Laby.labyAPI().minecraft().minecraftWindow().isScreenOpened()) {
             Laby.labyAPI().minecraft().setClipboard(addon.getSkullApi().getCopy(addon.configuration()));
             Laby.references().chatExecutor().displayClientMessage(I18n.translate("headowner.messages.copied"));
         }
