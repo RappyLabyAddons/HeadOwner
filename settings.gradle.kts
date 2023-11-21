e899766d2899e0b758105a1da963cb6dafcdb5d4
RappyTV <rappytv@rappytv.com>
2023-11-21 19:56:15 +0100

    Bump labygradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "HeadOwner"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.29"
+    val labyGradlePluginVersion = "0.3.31"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
